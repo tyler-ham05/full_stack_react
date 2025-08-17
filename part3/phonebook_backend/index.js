31087d29cb9bca18a11e59a0aff5dfc899554b11
tyler-ham05 <tyjham05@gmail.com>
2025-08-16 18:39:45 -0700

    port issue

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -82,9 +82,10 @@ app.post("/api/persons", (request, response) => {
    
 })
 
-const PORT = 3001
-app.listen(PORT)
-console.log(`Server running on port ${PORT}`)
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
 
 
  
