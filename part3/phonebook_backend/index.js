16bac2a00ce279afc45185865dcd46dda62aa08e
tyler-ham05 <tyjham05@gmail.com>
2025-08-21 10:31:14 -0700

    uibuild

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -8,8 +8,8 @@ const Person = require('./modules/person')
 require('dotenv').config()
 
 
-const middleware = morgan.token('body', (req) => {
-    return req.method === 'POST' ? JSON.stringify(req.body) : ''
+morgan.token('body', (req) => {
+  return req.method === 'POST' ? JSON.stringify(req.body) : ''
 })
 
 const errorHandler = (error, request, response, next) => {
@@ -26,64 +26,69 @@ const errorHandler = (error, request, response, next) => {
 }
 
 
-app.use(morgan(':method :url :status :res[content-length] :response-time ms :body'))
 app.use(express.json())
+app.use(morgan(':method :url :status :res[content-length] :response-time ms :body'))
 app.use(express.static('dist'))
 app.use(cors())
-app.use(errorHandler)
 
 console.log("hello world")
 
 
-app.get("/info", (request, response) => {
-    Person.find({}).then(people => {response.send(`<div><p>Phonebook has info for ${people.length} people </p><p> </p></div>`)})
-    
+app.get("/info", (request, response, next) => {
+  Person.find({})
+    .then(people => {
+      response.send(`<div><p>Phonebook has info for ${people.length} people </p><p> </p></div>`)
+    })
+    .catch(error => next(error))
 })
 //${dayjs().format("dddd, MMMM D, YYYY, h:mm A")} does not work with server deployment
 
-app.get("/api/persons", (request, response) => {
-    Person.find({}).then(people => {
-        response.json(people)
+app.get("/api/persons", (request, response, next) => {
+  Person.find({})
+    .then(people => {
+      response.json(people)
     })
-    
+    .catch(error => next(error))
 })
 
-app.get("/api/persons/:id", (request, response) => {
-    Person.findById(request.params.id).then(person => {
-        response.json(person)
+app.get("/api/persons/:id", (request, response, next) => {
+  Person.findById(request.params.id)
+    .then(person => {
+      if (!person) {
+        return response.status(404).end()
+      }
+      response.json(person)
     })
+    .catch(error => next(error))
 })
 
-app.delete("/api/persons/:id", (request, response) => {
-    Person.findByIdAndDelete(request.params.id)
-    .then(result =>{response.status(204).end()})
-    .catch(error => next(error)) 
+app.delete("/api/persons/:id", (request, response, next) => {
+  Person.findByIdAndDelete(request.params.id)
+    .then(() => { response.status(204).end() })
+    .catch(error => next(error))
 })
 
-app.post("/api/persons", (request, response) => {
-    const body = request.body
-    if (!body.name){
-        return response.status(400).json({error: 'name missing'})
-    }
-    else{
-        const person = new Person({
-            name: body.name,
-            number: body.number
-        })
+app.post("/api/persons", (request, response, next) => {
+  const body = request.body
+  if (!body.name) {
+    return response.status(400).json({ error: 'name missing' })
+  }
 
-        person
-            .save()
-            .then(newPerson => {
-                response.json(newPerson)
-            })
-            .catch(error =>
-                next(error)
-            )
-        }
+  const person = new Person({
+    name: body.name,
+    number: body.number
+  })
+
+  person
+    .save()
+    .then(newPerson => {
+      response.json(newPerson)
     })
+    .catch(error => next(error))
+})
 
-app.put("/api/persons/:id", (request, response) => {
-     const { name, number } = request.body
+app.put("/api/persons/:id", (request, response, next) => {
+  const { name, number } = request.body
 
   Person.findById(request.params.id)
     .then(person => {
@@ -99,7 +104,10 @@ app.put("/api/persons/:id", (request, response) => {
       })
     })
     .catch(error => next(error))
-}) 
+})
+
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
